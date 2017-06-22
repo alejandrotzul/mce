fba1be914fdb07de218b3de5944bfdcd91c3a2a7
mhernandez <mhernandez@dca.gob.gt>
2017-06-22 13:25:31 -0600

    inicio de mce
    Signed-off-by: mhernandez <mhernandez@dca.gob.gt>

---
+++ tmp/visual/footer.php
@@ -0,0 +1,5 @@
+<BR><BR><BR><BR>
+<HR style="color: rgb(0, 0, 0)" noShade="noshade">
+
+<P align="center"> © 2016 Diario de Centro América - Todos los derechos   
+ reservados.</P>
---
+++ tmp/visual/header.php
@@ -0,0 +1,1 @@
+<P align="center"><IMG border="0" align="center" src="images/logodca.png" height="100"></P>
